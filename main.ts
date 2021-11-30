115956b5fd6d8b1fc1dd5205e060241077c93dad
PatoGMTP <93350228+PatoGMTP@users.noreply.github.com>
2021-11-30 11:57:57 -0600

    Pre-Start

--- main.ts
+++ main.ts
@@ -30,3 +30,32 @@ Student
     Add a button that sorts the data based on Grade (ascending order)
     Add a button that sorts teh data based on Course (ascending order)
 */
+
+interface StudentInt
+{
+    first_name: string,
+    last_name: string,
+    course: string,
+    grade: number | string,
+    is_passing: boolean,
+    check_passing(): void,
+    update_grade(grade: number): void
+}
+
+class Student implements StudentInt
+{
+    static passing_grades: string[] = ["A", "B", "C", "A+", "B+", "C+", "A-", "B-", "C-"];
+
+    minimum_grade: number = "70";
+    is_passing: boolean;
+    constructor (public first_name: string, public last_name: string, public course: string, public grade: number | string)
+    {
+        this.che
+    }
+
+    check_passing(): void
+    {
+        // if ()
+        this.is_passing = true;
+    }
+}
